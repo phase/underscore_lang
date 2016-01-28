d810ec8878de7442c1ad7436a10ab43a80486e42
phase <ph4sesaber@gmail.com>
2016-01-27 17:06:30 -0800

    Tokens and Identifier checking

--- src/lang/lexer.d
+++ src/lang/lexer.d
@@ -1,11 +1,25 @@
 import std.stdio;
 
+class Token {
+    // Base Token Class
+}
+
+class Identifier : Token {
+    string identifier;
+    
+    this(string s) {
+        identifier = s;
+    }
+}
+
 void parse(char[] prog) {
+    Token[] tokens;
     //flags
     bool comment;
     bool multiComment;
     //vars for storing stuff
     char last;
+    char[] buf;
     foreach(c; prog) {
         //Comments
         if(multiComment) {
@@ -24,10 +38,22 @@ void parse(char[] prog) {
                 multiComment = true;
             }
         }
+        else if(alphnum(c)) {
+            buf ~= c;
+        }
+        else if(c == ' ') {
+            if(buf != "") {
+                tokens ~= new Identifier(buf);
+            }
+        }
         else {
             //TODO: not sure what should be here
             write(c);
         }
         last = c;
     }
+}
+
+bool alphnum(char c) {
+    return (c>='a' && c<='z') || (c>='A' && c<='Z') || (ch>='0' && ch<='9'))
 }
